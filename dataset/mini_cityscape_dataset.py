48e8da2d7bdfa2c309749c3f0dfdc74fad270e90
Tomáš Majerčík <tomas.majercik2@gmail.com>
2025-11-30 16:29:16 +0100

    working training

--- dataset/mini_cityscape_dataset.py
+++ dataset/mini_cityscape_dataset.py
@@ -5,7 +5,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class MiniCityscapeDataset(Dataset):
-    def __init__(self, root, split='train', transform=None):
+    def __init__(self, root, split='train', transform=None, max_samples=None):
         self.root = root
         self.split = split
         self.transform = transform
@@ -13,18 +13,24 @@ class MiniCityscapeDataset(Dataset):
         self.images = sorted(os.listdir(os.path.join(root, split, 'input')))
         self.masks = sorted(os.listdir(os.path.join(root, split, 'masks')))
 
+        if max_samples is not None:
+            self.images = self.images[:max_samples]
+            self.masks = self.masks[:max_samples]
+
     def __len__(self):
         return len(self.images)
     
-    def __getitem__(self, idx):
+    def __getitem__(self, idx, target_size=(512, 256)):
         img_path = os.path.join(self.root, self.split, 'input', self.images[idx])
         mask_path = os.path.join(self.root, self.split, 'masks', self.masks[idx])
 
-        image = np.array(Image.open(img_path).convert("RGB"))
-        mask = np.array(Image.open(mask_path))
+        image = np.array(Image.open(img_path).convert("RGB").resize(target_size))
+        mask  = np.array(Image.open(mask_path).resize(target_size, resample=Image.NEAREST))
 
         # convert to tensors
         image = torch.tensor(image).permute(2, 0, 1).float() / 255.0  # Normalize to [0, 1]
         mask = torch.tensor(mask).long()
 
+        # Ignore all labels >= 32
+        mask[mask >= 32] = 255
         return image, mask
--- infer.py
+++ infer.py
@@ -1,12 +1,13 @@
 import torch
-from PIL import Image
 import numpy as np
+from PIL import Image
 from models.unet import UNet
+import matplotlib.pyplot as plt
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-model = UNet(n_classes=34).to(DEVICE)
-model.load_state_dict(torch.load("unet_cityscape.pth"))
+model = UNet(n_classes=32).to(DEVICE)
+model.load_state_dict(torch.load("unet_weights3.pth", map_location=torch.device(DEVICE)))
 model.eval()
 
 def infer(img_path):
@@ -17,7 +18,23 @@ def infer(img_path):
     with torch.no_grad():
         pred = model(tensor).argmax(1).squeeze().cpu().numpy()
 
-    return pred
+    return img, pred
+
+def plot_segmentation(img, pred):
+    plt.figure(figsize=(12,5))
+
+    plt.subplot(1,2,1)
+    plt.title("Original Image")
+    plt.imshow(img)
+    plt.axis('off')
+
+    plt.subplot(1,2,2)
+    plt.title("Predicted Segmentation")
+    plt.imshow(pred, cmap='tab20')  # or any colormap
+    plt.axis('off')
+
+    plt.show()
 
 if __name__ == "__main__":
-    infer("data/mini-cityscape/val/input/00000.png")
+    img, pred = infer("data/mini-cityscape/val/input/00000.png")
+    plot_segmentation(img, pred)
